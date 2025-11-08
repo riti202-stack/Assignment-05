b1ccb04e980c8accfa65c5d4ae3e866c378f991a
riti <abidariti690@gmail.com>
2025-11-08 08:50:18 +0600

    navbar completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-start': '#013223', // Your desired starting hex color
+        'custom-middle': '#001133', // Optional: for a three-stop gradient
+        'custom-end': '#013223',   // Your desired ending hex color
+      },
+    },
+  },
+  plugins: [],
+}
